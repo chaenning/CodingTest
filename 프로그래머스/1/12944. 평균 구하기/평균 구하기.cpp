c66c6ecf1d6096cdc76d68a341d216590d3f2b03
chaenning <84309619+chaenning@users.noreply.github.com>
2025-05-30 09:19:55 +0900

    [level 1] Title: 평균 구하기, Time: 0.01 ms, Memory: 4.06 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.cpp
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.cpp
@@ -6,7 +6,9 @@ using namespace std;
 
 double solution(vector<int> arr) {
     double answer = 0;
-    answer = (accumulate(arr.begin() , arr.end(), 0.0) ) / arr.size();
+    
+    answer = accumulate(arr.begin() , arr.end(), 0) ;
+    answer = answer / arr.size();
     
     return answer;
 }
